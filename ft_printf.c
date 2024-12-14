a57cf386eebdb78995bd4b792a67a77744340414
Megumi Sasaki <mesasaki@c4r1s9.42tokyo.jp>
2024-12-14 14:37:40 +0900

    aa

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: mesasaki <mesasaki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/17 21:31:26 by mesasaki          #+#    #+#             */
-/*   Updated: 2024/06/18 04:10:33 by mesasaki         ###   ########.fr       */
+/*   Updated: 2024/12/14 14:36:49 by mesasaki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,6 @@ int	support_printf(const char *format, va_list args)
 		count += pr_unsbase(va_arg(args, unsigned int), format);
 	else if (*format == 'p')
 	{
-		count += pr_0x();
 		count += pr_putpointer(va_arg(args, uintptr_t));
 	}
 	else
@@ -62,62 +61,82 @@ int	ft_printf(const char *format, ...)
 	return (count);
 }
 
-int	main(void)
-{
-	int i;
-	int original;
-	int *point;
-	int test;
+// int	main(void)
+// {
+// 	int i;
+// 	int original;
+// 	int *point;
+// 	int test;
 
-	test = 3;
-	point = &test;
+// 	test = 3;
+// 	point = &test;
 
-	i = ft_printf("%c\n", 'A');
-	original = printf("%c\n", 'A');
-	printf("%i\n", i);
-	printf("%i\n", original);
+// 	i = ft_printf("%c\n", 'A');
+// 	original = printf("%c\n", 'A');
+// 	printf("%i\n", i);
+// 	printf("%i\n", original);
 
-	i = ft_printf("%d\n", -12345);
-	original = printf("%d\n", -12345);
-	printf("%d\n", i);
-	printf("%d\n", original);
+// 	i = ft_printf("%d\n", -12345);
+// 	original = printf("%d\n", -12345);
+// 	printf("%d\n", i);
+// 	printf("%d\n", original);
 
-	i = ft_printf("%s\n", "ABCDE");
-	original = printf("%s\n", "ABCDE");
-	printf("%d\n", i);
-	printf("%d\n", original);
+// 	i = ft_printf("%s\n", "ABCDE");
+// 	original = printf("%s\n", "ABCDE");
+// 	printf("%d\n", i);
+// 	printf("%d\n", original);
 
-	i = ft_printf("%u\n", 456);
-	original = printf("%u\n", 456);
-	printf("%d\n", i);
-	printf("%d\n", original);
+// 	i = ft_printf("%u\n", 456);
+// 	original = printf("%u\n", 456);
+// 	printf("%d\n", i);
+// 	printf("%d\n", original);
 
-	i = ft_printf("%x\n", 255);
-	original = printf("%x\n", 255);
-	printf("%x\n", i);
-	printf("%x\n", original);
+// 	i = ft_printf("debug:%u\n", -456);
+// 	original = printf("debug:%u\n", -456);
+// 	printf("%d\n", i);
+// 	printf("%d\n", original);
 
-	i = printf("%p\n", &point);
-	original = ft_printf("%p\n", &point);
-	printf("%d\n", i);
-	printf("%d\n", original);
+// 	i = ft_printf("%x\n", 255);
+// 	original = printf("%x\n", 255);
+// 	printf("%x\n", i);
+// 	printf("%x\n", original);
 
-	i = printf("%%aaaaa\n");
-	original = ft_printf("%%aaaaa\n");
-	printf("%d\n", i);
-	printf("%d\n", original);
+// 	i = ft_printf("%x\n", -123);
+// 	original = printf("%x\n", -123);
+// 	printf("%d\n", i);
+// 	printf("%d\n", original);
 
-	i = printf("%% %%\n");
-	original = ft_printf("%% %%\n");
-	printf("%d\n", i);
-	printf("%d\n", original);
+// 	i = ft_printf("%X\n", -123);
+// 	original = printf("%X\n", -123);
+// 	printf("%d\n", i);
+// 	printf("%d\n", original);
 
-	i = printf("NULL %s NULL \n", "");
-	original = ft_printf("NULL %s NULL \n", "");
-	printf("%d\n", i);
-	printf("%d\n", original);
+// 	i = printf("%p\n", &point);
+// 	original = ft_printf("%p\n", &point);
+// 	printf("%d\n", i);
+// 	printf("%d\n", original);
 
-	ft_printf("Hello %s! This is a number: %d and a char: %c\n", "World", 42,
-		'A');
-	return (0);
-}
+// 	i = printf("%%aaaaa\n");
+// 	original = ft_printf("%%aaaaa\n");
+// 	printf("%d\n", i);
+// 	printf("%d\n", original);
+
+// 	i = printf("%% %%\n");
+// 	original = ft_printf("%% %%\n");
+// 	printf("%d\n", i);
+// 	printf("%d\n", original);
+
+// 	i = printf("NULL %s NULL \n", "");
+// 	original = ft_printf("NULL %s NULL \n", "");
+// 	printf("%d\n", i);
+// 	printf("%d\n", original);
+
+// 	ft_printf("Hello %s! This is a number: %d and a char: %c\n", "World", 42,
+// 		'A');
+
+// 	i = printf("debug:%p\n", NULL);
+// 	original = ft_printf("debug:%p\n", NULL);
+// 	printf("%d\n", i);
+// 	printf("%d\n", original);
+// 	return (0);
+// }
--- pr_putpointer.c
+++ pr_putpointer.c
@@ -6,18 +6,22 @@
 /*   By: mesasaki <mesasaki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/17 21:31:59 by mesasaki          #+#    #+#             */
-/*   Updated: 2024/06/18 04:01:39 by mesasaki         ###   ########.fr       */
+/*   Updated: 2024/12/14 14:34:44 by mesasaki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	pr_0x(void)
+int	pr_putpointer_recursive(uintptr_t ptr)
 {
 	int	count;
 
 	count = 0;
-	count += pr_putstr("0x");
+	if (ptr >= 16)
+	{
+		count += pr_putpointer_recursive(ptr / 16);
+	}
+	count += pr_putchar("0123456789abcdef"[ptr % 16]);
 	return (count);
 }
 
@@ -27,12 +31,11 @@ int	pr_putpointer(uintptr_t ptr)
 
 	count = 0;
 	if (ptr == 0)
-		count += pr_putchar('0');
+		count += pr_putstr("(nil)");
 	else
 	{
-		if (ptr >= 16)
-			count += pr_putpointer(ptr / 16);
-		count += pr_putchar("0123456789abcdef"[ptr % 16]);
+		count += pr_putstr("0x");
+		count += pr_putpointer_recursive(ptr);
 	}
 	return (count);
 }
