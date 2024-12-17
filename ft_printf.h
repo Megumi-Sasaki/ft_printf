5cd67f3995e71db0200a5c26f36aa8d283380043
Cacapon Project <cacapon.project@gmail.com>
2024-12-17 15:43:41 +0900

    エラーハンドリング対応

--- ft_printf.h
+++ ft_printf.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mesasaki <mesasaki@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/17 21:32:49 by mesasaki          #+#    #+#             */
-/*   Updated: 2024/06/18 04:07:16 by mesasaki         ###   ########.fr       */
+/*   Updated: 2024/12/17 15:37:51 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@
 # include <unistd.h>
 
 int	ft_printf(const char *format, ...);
-int	support_printf(const char *format, va_list args);
+int	support_printf(const char *format, va_list args, size_t count);
 int	pr_putchar(char c);
 int	pr_putnbr(int n);
 int	pr_putstr(const char *s);
@@ -28,6 +28,6 @@ int	pr_put_unsigned(unsigned int n);
 int	pr_puthex(unsigned int n, int uppercase);
 int	pr_putpointer(uintptr_t ptr);
 int	pr_0x(void);
-int	ft_putstr_fd(const char *s);
+int	ft_putstr_fd(const char *s, size_t count);
 
 #endif
